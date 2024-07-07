b035c858de880a2cf1d5c696b194b8091e90352c
isabeldiana <isabel_diana@outlook.com>
2024-07-06 22:45:02 -0300

    create: Creation decryption and copy

--- src/app.js
+++ src/app.js
@@ -1,31 +1,69 @@
-const textarea = document.getElementById("textoInput");
+const textarea = document.getElementById("textInput");
 
-const criptografar = ()=>{
-  let fraseCriptografada = '';
-  let texto = textarea.value; 
-  const substituicao = {
-    'i': 'imes', 
+const encrypText = () => {
+  let encryptedPhrase = '';
+  let text = textarea.value;
+  const replacement = {
+    'i': 'imes',
     'a': 'ai',
     'o': 'ober',
     'u': 'ufat'
   };
-  for( i = 0 ; i < texto.length; i++){
-    let letra = texto[i];
-    if(substituicao[letra]){
-      fraseCriptografada += substituicao[letra]
-    }else {
-      fraseCriptografada += letra;
+
+  if (/^[a-z]+$/.test(text)) {
+    for (i = 0; i < text.length; i++) {
+      let char = text[i];
+      if (replacement[char]) {
+        encryptedPhrase += replacement[char]
+      } else {
+        encryptedPhrase += char;
+      }
     }
-  
- 
-    
+  } else {
+    const contentFooterP = document.querySelector('.content-input-footer-p');
+    contentFooterP.style.color = 'red';
   }
-console.log(fraseCriptografada);
-  return fraseCriptografada;
+
+  const result = document.getElementById('result-content');
+  result.textContent = encryptedPhrase;
+  if (/^[a-z]+$/.test(text)) {
+    const contentResultHidden = document.querySelector('.content__result-section-hidden');
+    contentResultHidden.style.display = 'flex';
+
+    const contentResult = document.querySelector('.content__result-section');
+    contentResult.style.display = 'none';
+  }
+  return encryptedPhrase;
 
 }
 
 
-let resultadoCriptografado = criptografar();
-console.log(resultadoCriptografado); 
+const decryptText = () => {
+  let phrasesDecrypted = '';
+  let text = textarea.value;
+  const replacement = {
+    'imes': 'i',
+    'ai': 'a',
+    'ober': 'o',
+    'ufat': 'u'
+  };
+  for (let [key, value] of Object.entries(replacement)) {
+    text = text.split(key).join(value);
+  }
+  phrasesDecrypted = text;
 
+  const result = document.getElementById('result-content');
+  result.textContent = phrasesDecrypted;
+
+  const contentResultHidden = document.querySelector('.content__result-section-hidden');
+  contentResultHidden.style.display = 'flex';
+
+  const contentResult = document.querySelector('content__result-section');
+  contentResult.style.display = 'none';
+  return phrasesDecrypted;
+}
+
+const copyText = () => {
+  let textCopy = document.getElementById('result-content').innerText;
+  navigator.clipboard.writeText(textCopy)
+}
